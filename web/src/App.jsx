7f48ae36200bd5f9987ffed98fe3faa9c6e08704
Ritchie Jay Erag <eragritchiegg@gmail.com>
2026-07-08 14:31:25 +0800

    Refactor User Management into a feature slice on web (features/users)

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from './features/auth/ProtectedRoute'
 import LoginPage from './features/auth/LoginPage'
 import POSPage from './features/pos/POSPage'
-import ManageUsersPage from './pages/ManageUsersPage'
+import ManageUsersPage from './features/users/ManageUsersPage'
 import SalesHistoryPage from './pages/SalesHistoryPage'
 
 export default function App() {
